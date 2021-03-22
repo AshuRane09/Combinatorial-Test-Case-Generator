c8122545998bd346245bd836a63508f8b058d459
Ashutosh Rane <raneashutosh09@gmail.com>
2021-03-22 21:38:27 +0530

    python script

--- pyScript/pso.py
+++ pyScript/pso.py
@@ -1,6 +1,4 @@
 from PSO.PSO import runPSO
-from TLBO.TLBO import runTLBO
-from SA.simulation_annealing import runSA
 from txtcsv import conversion
 from Final import Final
 
--- pyScript/tlbo.py
+++ pyScript/tlbo.py
@@ -1,9 +1,7 @@
-from PSO.PSO import runPSO
 from TLBO.TLBO import runTLBO
-from SA.simulation_annealing import runSA
 from txtcsv import conversion
 from Final import Final
 
 runTLBO()
 conversion()
-# Final()
+Final()
